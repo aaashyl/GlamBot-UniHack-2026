8ecf6f23404d0d30c3cc745869610dd55b1994eb
julie672 <you@example.com>
2026-03-14 17:30:27 +1100

    fixed my app.js conflict merge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,27 @@
-// import './App.css';
-
-// import WelcomePage from "./components/WelcomePage/WelcomePage";
-
-// function App() {
-//   return (
-//     <div>
-//       <WelcomePage />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
 
-import ProductDiscovery from "./components/ProductDiscoveryPage/ProductDiscoveryPage";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
 
 function App() {
   return (
     <div>
-      <ProductDiscovery />
+      <WelcomePage />
     </div>
   );
 }
 
 export default App;
+
+// import './App.css';
+
+// import ProductDiscovery from "./components/ProductDiscoveryPage/ProductDiscoveryPage";
+
+// function App() {
+//   return (
+//     <div>
+//       <ProductDiscovery />
+//     </div>
+//   );
+// }
+
+// export default App;
