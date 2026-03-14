3477b990c195003d42878e5a3d50163943f06ff1
Prisha Sharda <prishasharda15@gmail.com>
2026-03-14 18:19:03 +1100

    temp change to make survey work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,28 @@
-import './App.css';
+// import './App.css';
 
-import WelcomePage from "./components/WelcomePage/WelcomePage";
+// import WelcomePage from "./components/WelcomePage/WelcomePage";
+
+
+// function App() {
+//   return (
+//     <div>
+//       <WelcomePage />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import './App.css';
+import SurveyPage from './components/SurveyPage/SurveyPage';
 
 function App() {
   return (
     <div>
-      <WelcomePage />
+      <SurveyPage />
     </div>
   );
 }
 
 export default App;
-
